12b1d1bb05ad68f536e655484d3c482fe666c87b
Alexis Aranda Ocampo <alexis_aranda_ocampo@hotmail.com>
2017-09-26 14:55:18 -0300

    Listo soldado

---
+++ src/warGame/Soldado.java
@@ -0,0 +1,26 @@
+package warGame;
+
+public class Soldado extends Unidad {
+
+	public Soldado() {
+		
+	}
+	
+	@Override
+	public void tomarAgua() {
+		
+	}
+	
+	@Override
+	protected boolean puedeAtacar() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected int hacerDanio() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
