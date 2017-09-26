d20b3e9a531fd7b28ac98575468f0728aadb6d11
Alexis Aranda Ocampo <alexis_aranda_ocampo@hotmail.com>
2017-09-26 14:51:14 -0300

    Listo unidad

--- src/warGame/Unidad.java
+++ src/warGame/Unidad.java
@@ -2,4 +2,25 @@ package warGame;
 
 public abstract class Unidad {
 
+	protected int salud;
+	private int posicionX;
+	private int posicionY;
+	
+	public void atacar(Unidad that) {
+		
+	}
+	protected abstract boolean puedeAtacar();
+	protected abstract int hacerDanio();
+	protected void recibirDanio(int danio) {
+		
+	}
+	protected double distanciaA(Unidad that) {
+		return 0;	
+	}
+	public void tomarAgua() {
+		
+	}
+	public boolean estaVivo() {
+		return true;
+	}
 }
