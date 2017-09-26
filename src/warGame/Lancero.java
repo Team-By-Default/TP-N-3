311337cf540d4f5e5d6e92cb2c6f747c99a14357
Alexis Aranda Ocampo <alexis_aranda_ocampo@hotmail.com>
2017-09-26 14:58:18 -0300

    listo Lancero

---
+++ src/warGame/Lancero.java
@@ -0,0 +1,21 @@
+package warGame;
+
+public class Lancero extends Unidad {
+
+	public Lancero() {
+		
+	}
+	
+	@Override
+	protected boolean puedeAtacar() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected int hacerDanio() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
