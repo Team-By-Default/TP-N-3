2a8f537b2a0fb98976dec8f5241d157e53efaa0c
Daniel Mariano Varela <danielmvarela@hotmail.com>
2017-09-27 10:24:30 -0300

    Definimos Arquero
    Definimos a la Unidad Arquero

--- src/warGame/Arquero.java
+++ src/warGame/Arquero.java
@@ -2,25 +2,55 @@ package warGame;
 
 public class Arquero extends Unidad {
 
+	/**
+	 * La cantidad de flechas que posee el personaje.
+	 */
 	int flechas;
+
+	/**
+	 * Un ARQUERO tiene, de inicial y tope, 
+	 * una salud de 50 y hace un
+	 * danio de 5.
+	 */
+	private static final int SALUDTOPE = 50;
+	private static final int DANIO = 5;
+	
 	
 	public Arquero() {
+		this.salud = SALUDTOPE;
+		this.danio = DANIO;
 		flechas=20;
 	}
 	
+	/**
+	 * El arquero carga 6 flechas, las cuales vienen en un paquete, y las carga en el carcaj
+	 */
 	public void cargarFlechas() {
-		
+		flechas += 6;
 	}
-
+	
+	/**
+	 * Al inflingir danio, el arquero debe
+	 * utilizar una flecha.
+	 * @return El daño realizado
+	 */
 	@Override
 	protected float hacerDanio() {
-		// TODO Auto-generated method stub
-		return 0;
+		flechas --;
+		return DANIO;
 	}
 
+	/**
+	 * Un arquero puede atacar si tiene flechas
+	 * y si se encuentra de 2 a 5 de distancia del
+	 * objetivo.
+	 * @param that Es la Unidad a la que pretende atacar
+	 * @return true si puede atacar, false si no puede
+	 */
 	@Override
 	protected boolean puedeAtacar(Unidad that) {
-		// TODO Auto-generated method stub
+		if(distanciaA(that) >= 2 && distanciaA(that) <= 5 && flechas > 0)
+			return true;
 		return false;
 	}
 
