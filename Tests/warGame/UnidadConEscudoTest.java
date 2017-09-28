4f996810e4ba3ad33cceffb61e0e6f00b20ab1dc
Daniel Mariano Varela <danielmvarela@hotmail.com>
2017-09-28 17:12:41 -0300

    Varias modificaciones en Unidad con escudo

--- Tests/warGame/UnidadConEscudoTest.java
+++ Tests/warGame/UnidadConEscudoTest.java
@@ -13,21 +13,52 @@ public class UnidadConEscudoTest {
 	
 	@Before
 	public void setUp() {
-	alexis = new UnidadConEscudo(new Soldado(1,0));
-	valeria = new UnidadConEscudo(new Caballero(0,0));
-	daniel = new UnidadConEscudo(new Arquero(0,0));
-	lara = new UnidadConEscudo(new Lancero(0,0));
+	alexis = new UnidadConEscudo(new Soldado());
+	valeria = new UnidadConEscudo(new Caballero(1,0));
+	daniel = new UnidadConEscudo(new Arquero(2,0));
+	lara = new UnidadConEscudo(new Lancero(1,0));
 	}
 	
+	/**
+	 * Vemos si los atributos de cada rama estan bien ubicados
+	 */
 	@Test
 	public void testeandoAtributos() {
 
-		//Assert.assertEquals(100, ((Soldado)alexis).getEnergia());
-	}
+		Assert.assertEquals(200, alexis.getSalud(),0);
+		Assert.assertEquals(true, alexis.estaVivo());
 
+		Assert.assertEquals(200, valeria.getSalud(),0);
+		Assert.assertEquals(true, valeria.estaVivo());
+
+		Assert.assertEquals(150, lara.getSalud(),0);
+		Assert.assertEquals(true, lara.estaVivo());
+
+		Assert.assertEquals(50, daniel.getSalud(),0);
+		Assert.assertEquals(true, daniel.estaVivo());
+	}
+	
+	/**
+	 * Vemos si el daño esta bien aplicado
+	 */
 	@Test
-	public void testeandoCombate() {
-		Assert.assertEquals(true, alexis.atacar(daniel));
-		Assert.assertEquals(46, daniel.getSalud(), 0);
+	public void testeandoDanio() {
+		Assert.assertEquals(50, daniel.getSalud(),0);
+		Assert.assertEquals(true, valeria.atacar(daniel));
+		Assert.assertEquals(30, daniel.getSalud(),0);
+		
+		Assert.assertEquals(200, valeria.getSalud(),0);
+		Assert.assertEquals(true, daniel.atacar(valeria));
+		Assert.assertEquals(198, valeria.getSalud(),0);
+		
+
+		Assert.assertEquals(200, alexis.getSalud(),0);
+		Assert.assertEquals(true, lara.atacar(alexis));
+		Assert.assertEquals(190, alexis.getSalud(),0);
+		
+		Assert.assertEquals(150, lara.getSalud(),0);
+		Assert.assertEquals(true, alexis.atacar(lara));
+		Assert.assertEquals(146, lara.getSalud(),0);
+		
 	}
 }
